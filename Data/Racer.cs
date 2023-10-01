4c2a8a861646202b56bdf261f233fb803978ed17
Ihavedremgio <emre.c.school@gmail.com>
2023-10-01 17:23:31 +0200

    update 1.6 :)

--- Data/Racer.cs
+++ Data/Racer.cs
@@ -7,16 +7,22 @@ namespace Ski.Data
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Geslacht { get; set; }
+    public string CurrentYear = DateTime.Now.Year.ToString();
+    public string ClubCode = "HOO";
+    public string Club = "Skiteam Hoofddorp";
+
     public DateOnly? Geboortejaar { get; set; }
     public string? Categorie { get; set; }
 
-    public TimeOnly Run1 { get; set; }
-    public TimeOnly Run2 { get; set; }
-    public TimeOnly Run3 { get; set; }
-    
+    public SkiTime? Run1 { get; set; }
+    public SkiTime? Run2 { get; set; }
+    public SkiTime? Run3 { get; set; }
+
+    public SkiTime? FinalTime { get; set; }
+
   }
 
-  
+
 
 
 }
---
+++ Data/SkiTime.cs
@@ -0,0 +1,20 @@
+﻿namespace Ski.Data
+{
+  public class SkiTime 
+  { 
+    public int Minutes { get; set; }
+    public int Seconds { get; set; }
+    public bool DNQ { get; set; }
+    public int TotalSeconds => + (Minutes * 60) + Seconds;
+
+    public SkiTime AddTime(SkiTime otherTime)
+    {
+      int totalSeconds = TotalSeconds + otherTime.TotalSeconds;
+
+      int minutes = totalSeconds / 60;
+      int seconds = totalSeconds % 60;
+
+      return new SkiTime { Minutes = minutes, Seconds = seconds };
+    }
+  }
+}
